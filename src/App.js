5a3b11bfe0d667c59c30f6da8eec6e3d2d949dbb
deucenn <panix.black@web.de>
2024-03-18 09:59:00 +0100

    build: past projects

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import "./App.css";
 import Title from "./components/Title";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
 
 function App() {
   return <>
+    <Nav/>
+    <About/>
+    <Projects/>
+    <Skills/>
+    <Contact/>
     <Title/>
   </>;
 }
