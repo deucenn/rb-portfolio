a610a001c8b66c324e7106a5700a32f61045e655
deucenn <panix.black@web.de>
2024-03-18 12:30:42 +0100

    update: skills

--- src/Data.js
+++ src/Data.js
@@ -1,34 +1,41 @@
 export const projects = [
-    {
-      title: "React Reserve",
-      subtitle: "MERN Stack",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-1.gif",
-      link: "https://reactbootcamp.com",
-    },
-    {
-      title: "React Tracks",
-      subtitle: "React and Python",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-2.gif",
-      link: "https://reedbarger.com",
-    },
-    {
-      title: "DevChat",
-      subtitle: "React and Firebase",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-3.gif",
-      link: "https://jsbootcamp.com",
-    },
-    {
-      title: "Epic Todo App",
-      subtitle: "React Hooks",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-4.gif",
-      link: "https://pythonbootcamp.com",
-    },
-  ];
+  {
+    title: "Weather App",
+    subtitle: "HTML//CSS//JS",
+    description: "A weather application using the openweather api.",
+    image: "./assets/weather-app.png",
+    link: "https://verdant-custard-960952.netlify.app/",
+  },
+  {
+    title: "Currency Converter",
+    subtitle: "HTML//CSS//JS",
+    description: "A currency converter using the exchangerate api.",
+    image: "./assets/currency-converter.png",
+    link: "https://currency-converter-rb.netlify.app/",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://jsbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://pythonbootcamp.com",
+  },
+];
+
+export const skills = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "TailwindCSS",
+    "Bootstrap",
+    "React"
+]
