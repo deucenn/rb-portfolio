9cdf5c560ed578895cc45c5968113d2f83249763
deucenn <panix.black@web.de>
2024-03-18 14:56:11 +0100

    fix: deleted unnecessary code

--- src/components/About.js
+++ src/components/About.js
@@ -19,16 +19,6 @@ function About() {
             Laborum, voluptas natus?
           </p>
           <div className="flex justify-center space-x-4">
-            {/* <a
-                href="#contact"
-                className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-                Contact Me
-              </a>
-              <a
-                href="#projects"
-                className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-                See My Past Work
-              </a> */}
             <a href="https://github.com/deucenn" className="hover:text-gray-200 hover:text-lg">
               <FaGithub />
             </a>
