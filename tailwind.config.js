6b16822605ec4c4e9d5debb4df91006c90b70e03
deucenn <panix.black@web.de>
2024-04-03 15:29:49 +0200

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-  extend: {},
+  extend: {
+    boxShadow: {
+      custom: "4px 4px 0 0 #8b5cf6",
+    },
+  },
   },
   plugins: [],
 };
